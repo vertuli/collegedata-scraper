ad1c039bd17e132afb77823fd0dcb31d73035d9b
Michael F Vertuli <michael@vertuli.com>
2018-12-11 00:16:23 -0800

    Separated cleaning and extraction functions.

---
+++ cleaning_functions.py
@@ -0,0 +1,140 @@
+import re
+
+def add_sub_labels(soup):
+    # Prepend parent labels to the <th> row label tags having class = 'sub'.
+    for tag in soup.find_all('th', class_ = 'sub'):
+        tag_text = tag.get_text(' ', strip = True)
+        parent_tag = tag.find_previous('th', class_ = None)
+        parent_text = parent_tag.get_text(' ', strip = True)
+        tag.string = parent_text + ', ' + tag_text
+    return soup
+
+
+def add_indent_labels(soup):
+    # Do the same for tags without class = 'sub' but with tab indents '\xa0':
+    regex = re.compile('\xa0')
+    for tag in soup.find_all('th', string = regex):
+        tag.string = tag.get_text(' ', strip = True)
+        header_tag = None
+        if tag.string == 'Women':
+            header_tag = tag.parent.find_previous('tr').th
+        if tag.string == 'Men':
+            header_tag = tag.parent.find_previous('tr').find_previous('tr').th
+        if header_tag:
+            tag.string = header_tag.string + ', ' + tag.string
+    return soup
+
+
+def add_gpa_labels(soup):
+    # Prepend labels to GPA row labels.
+    regex = re.compile('Grade Point Average')
+    caption = soup.find('caption', string = regex)
+    if caption:
+        gpa_table = caption.parent
+        header_tag = gpa_table.tbody.th
+        header_tag.string = 'GPA, Average'
+        sub_tags = gpa_table.find_all('th')[1:]
+        for tag in sub_tags:
+            tag.string = header_tag.string + ', ' + tag.string
+    return soup
+
+
+def add_finaid_prefixes(soup):
+    # Prepend labels to financial aid information
+    div = soup.find('div', id = 'section11')
+    if div:
+        for table in div.find_all('table')[0:2]:
+            caption_string = table.caption.get_text(' ', strip = True)
+            th_tags = table.find_all('th')
+            for tag in th_tags:
+                string = caption_string + ', '
+                string = string + tag.get_text(' ', strip = True)
+                tag.string = string
+    return soup
+
+
+def restructure_finaid_forms(soup):
+    # Restructure the strange Forms Required / Cost to File table.
+    th_tag = soup.find('th', string = "Forms Required")
+    if th_tag:
+        tbody_tag = th_tag.find_next('tbody')
+        for th in tbody_tag.find_all('th'):
+            if 'FAFSA' in th.text:
+                fafsa_code = th.text[-6:]
+                th.string = 'FAFSA Code'
+                td = th.find_next('td')
+                if td:
+                    td.string = fafsa_code
+    return soup
+
+
+def replace_hyperlinks(soup):
+    # Replace table data cells with links with the link address itself.
+    td_tags = soup.find_all('td')
+    for tag in td_tags:
+        link_tag = tag.find('a')
+        if link_tag:
+            tag.string = link_tag['href']
+    return soup
+
+
+def label_emails_websites(soup):
+    # Prepend labels to duplicate E-mail and Web Site labels.
+    regex = re.compile('Financial Aid Office')
+    caption = soup.find('caption', string = regex)
+    if caption:
+        email_tag = caption.find_next('th', string = 'E-mail')
+        email_tag.string = 'Financial Aid Office E-mail'
+        website_tag = caption.find_next('th', string = 'Web Site')
+        website_tag.string = 'Financial Aid Office Web Site'
+    return soup
+
+
+def fix_population_label(soup):
+    # Fix city Population labels (varying label contains city name).
+    regex = re.compile('Population')
+    th_tag = soup.find('th', string = regex)
+    if th_tag:
+        th_tag.string = "City Population"
+    return soup
+
+
+def fix_entrance_difficulty(soup):
+    # Rename the duplicate Entrance Difficulty label on the Overview page.
+    div = soup.find('div', id = 'section0')
+    if div:
+        th_tag = div.find('th', string = 'Entrance Difficulty')
+        th_tag.string = 'Entrance Difficulty, Category'
+    return soup
+
+
+def prepend_other_app_reqs(soup):
+    # Prepend labels to 'Other Application Requirements' table.
+    caption = 'Other Application Requirements'
+    caption_tag = soup.find('caption', string = caption)
+    if caption_tag:
+        th_tags = caption_tag.parent.find_all('th')
+        for tag in th_tags:
+            text = tag.get_text(' ', strip = True)
+            tag.string = 'Application Requirement, ' + text
+    return soup
+
+
+def prepend_security(soup):
+    # Prepend labels to 'Security' table.
+    div = soup.find('div', id = 'section22')
+    if div:
+        th_tags = div.find_all('th')
+        for tag in th_tags:
+            text = tag.get_text(' ', strip = True)
+            tag.string = 'Security, ' + text
+    return soup
+
+
+def delete_map_widget(soup):
+    # Delete the row containing the map widget.
+    b_tag = soup.find('b', string = 'View Larger Map')
+    if b_tag:
+        parent_row = b_tag.find_parent('tr')
+        parent_row.decompose()
+    return soup
--- collegedata_scraper.py
+++ collegedata_scraper.py
@@ -1,358 +1,135 @@
-# CollegeData scraper.
-import numpy as np
+import json
+import sys
+import logging
+import requests
+import bs4
 import pandas as pd
-from requests import get
-from bs4 import BeautifulSoup, SoupStrainer
-from IPython.display import clear_output
 import re
+from inspect import getmembers, isfunction
+import cleaning_functions
+import extracting_functions
+cleaning_function_list = getmembers(cleaning_functions, isfunction)
+extracting_functions_list = getmembers(extracting_functions, isfunction)
 
 
-# DEFINITIONS
-##############################################################################
-# The URLs of interest on collegedata.com are broken down like this:
-URL_PT_1 = "https://www.collegedata.com/cs/data/college/college_pg0"
-# followed by a `school_id` number. Following that, there is:
-URL_PT_2 = "_tmpl.jhtml?schoolId="
-# Finally, there is a `page_id` number, ranging from 1 to 6, inclusive.
-PAGE_IDS = range(1, 7)
-# Not all possible `school_id` numbers corresponds to a school, but most do.
-# A page requested corresponding to a `school_id` with no school data will
-# load a page that has a <h1> tag heading with this string:
-EMPTY_H1 = "Retrieve a Saved Search"
-# At larger `school_id` values, especially over 1000, no-school pages are
-# returned more often. I'm fairly confident there are none over 5000.
-SCHOOL_ID_START = 1
-SCHOOL_ID_END = 5000
-SCHOOL_IDS = range(SCHOOL_ID_START, SCHOOL_ID_END + 1)
-# CollegeData.com has no problem with requests without headers, but we can
-# send a fake header anyway:
-HEADERS = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) "
-           "AppleWebKit/605.1.15 (KHTML, like Gecko) "
-           "Version/12.0 Safari/605.1.15"}
-# Missing values are commonly labeled with these strings:
-NA_VALS = ['Not reported', 'Not Reported']
-# Finally, we'll export our scraped data to a CSV. The scraper will check
-# if the CSV already exists, and if so, will adjust the `SCHOOL_ID_START` to
-# begin with the `school_id` after the highest already scraped in the CSV:
-PATH = "test_scraped.csv"
+# Get config values.
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+    
+# The SoupStrainer limits our HTML results to the <h1> and the content <div>.
+def strainer_filter(tag, attrs):
+    return tag == 'h1' or attrs.get('id') == 'tabcontwrap'
+strainer = bs4.SoupStrainer(strainer_filter)
 
 
-def get_soup(url, SoupStrainer = None):
-    """Gets soup! Checks for a good response, too.
-    """
+# Error logging.
+logging.basicConfig(
+    filename = config['PATHS']['ERROR_LOG'], 
+    filemode = 'w', 
+    level = logging.DEBUG
+)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+def get_soup(url):
     # Request page and check if page returned.
-    result = get(url, headers = HEADERS)
-    
+    result = requests.get(url, headers = config['HEADERS'])
     if result.status_code != 200:
-        raise PageRequestError
-        
+        msg = url + ' gave status code ' + result.status_code
+        print(msg)
+        logging.warning(msg)
+        raise Exception
     # Parse page text into BeautifulSoup object.
-    soup = BeautifulSoup(result.text, "lxml", parse_only = SoupStrainer)
+    soup = bs4.BeautifulSoup(result.text, "lxml", parse_only = strainer)
     return soup
 
 
-def get_collegedata_page(school_id, page_id):
-    """Gets a specific CollegeData.com soup, and checks if it is properly 
-    formed.
-    """
-    # Build URL.
-    url = URL_PT_1 + str(page_id) + URL_PT_2 + str(school_id)
-    
-    # The SoupStrainer limits our results to the title and the content div.
-    strainer = SoupStrainer(lambda tag, d:
-                            tag == 'h1' or d.get('id') == 'tabcontwrap')
-
-    soup = get_soup(url, strainer)
-    print(f'Getting {url}')
+def scrape_school(school_id):
+    # Extract and combine values from six pages associated w/school_id.
+    school_series = pd.Series(name = school_id)
+    for page_id in range(1, 7):
+        # Request and convert page to soup object.
+        url = config['URL']['PART1'] + str(page_id) \
+                + config['URL']['PART2'] + str(school_id)
+        soup = get_soup(url)
+        if soup.h1.string == config['EMPTY_H1']:
+            msg = 'School ID ' + str(school_id) + ' has no info.'
+            print(msg)
+            logging.info(msg)
+            raise IOError
+        print(f'Extracting school {school_id}, page {page_id}.')
         
-    return soup
+        # Use cleaning and extracting functions to get page values.
+        for cleaning_func_tuple in cleaning_function_list:
+            cleaning_func = cleaning_func_tuple[1]
+            soup = cleaning_func(soup)
+        page_series = pd.Series()
+        for extracting_func_tuple in extracting_functions_list:
+            extracting_func = extracting_func_tuple[1]
+            extracted_series = extracting_func(soup)
+            page_series = page_series.append(extracted_series) 
+        school_series = school_series.append(page_series)
 
+        school_series = remove_duplicates(school_series)
+    return school_series
 
-def relabel(page):
-    """This relabels page tags to avoid data loss due to duplication.
-    It also deletes a pesky javascript map that could get later sucked up.
-    """
-    # Prepend parent labels to the <th> row label tags having class = 'sub'.
-    for tag in page.find_all('th', class_ = 'sub'):
-        tag_text = tag.get_text(' ', strip = True)
-        parent_tag = tag.find_previous('th', class_ = None)
-        parent_text = parent_tag.get_text(' ', strip = True)
-        tag.string = parent_text + ', ' + tag_text
-
-    # Do the same for tags without class = 'sub' but with tab indents '\xa0':
-    regex = re.compile('\xa0')
-    for tag in page.find_all('th', string = regex):
-        tag.string = tag.get_text(' ', strip = True)
-        if tag.string == 'Women':
-            header_tag = tag.parent.find_previous('tr').th
-        if tag.string == 'Men':
-            header_tag = tag.parent.find_previous('tr').find_previous('tr').th
-        if header_tag:
-            tag.string = header_tag.string + ', ' + tag.string  
             
-    # Prepend labels to GPA row labels.
-    regex = re.compile('Grade Point Average')
-    caption = page.find('caption', string = regex)
-    if caption:
-        gpa_table = caption.parent
-        header_tag = gpa_table.tbody.th
-        header_tag.string = 'GPA, Average'
-        sub_tags = gpa_table.find_all('th')[1:]
-        for tag in sub_tags:
-            tag.string = header_tag.string + ', ' + tag.string
-            
-    # Prepend labels to financial aid information
-    div = page.find('div', id = 'section11')
-    if div:
-        for table in div.find_all('table')[0:2]:
-            caption_string = table.caption.get_text(' ', strip = True)
-            th_tags = table.find_all('th')
-            for tag in th_tags:
-                string = caption_string + ', '
-                string = string + tag.get_text(' ', strip = True)
-                tag.string = string
-
-    # Replace table data cells with links with the link address itself.
-    td_tags = page.find_all('td')
-    for tag in td_tags:
-        link_tag = tag.find('a')
-        if link_tag:
-            tag.string = link_tag['href']
-
-    # Prepend labels to duplicate E-mail and Web Site labels.
-    regex = re.compile('Financial Aid Office')
-    caption = page.find('caption', string = regex)
-    if caption:
-        email_tag = caption.find_next('th', string = 'E-mail')
-        email_tag.string = 'Financial Aid Office E-mail'
-        website_tag = caption.find_next('th', string = 'Web Site')
-        website_tag.string = 'Financial Aid Office Web Site'
-        
-    # Fix city Population labels (varying label contains city name).
-    regex = re.compile('Population')
-    th_tag = page.find('th', string = regex)
-    if th_tag:
-        th_tag.string = "City Population"
-        
-    # Rename the duplicate Entrance Difficulty label on the Overview page.
-    div = page.find('div', id = 'section0')
-    if div:
-        th_tag = div.find('th', string = 'Entrance Difficulty')
-        th_tag.string = 'Entrance Difficulty, Category'
-        
-    # Prepend labels to 'Other Application Requirements' table.
-    caption = 'Other Application Requirements'
-    caption_tag = page.find('caption', string = caption)
-    if caption_tag:
-        th_tags = caption_tag.parent.find_all('th')
-        for tag in th_tags:
-            text = tag.get_text(' ', strip = True)
-            tag.string = 'Application Requirement, ' + text
-        
-    # Prepend labels to 'Security' table.
-    div = page.find('div', id = 'section22')
-    if div:
-        th_tags = div.find_all('th')
-        for tag in th_tags:
-            text = tag.get_text(' ', strip = True)
-            tag.string = 'Security, ' + text
-    
-    # Delete the row containing the map widget.
-    b_tag = page.find('b', string = 'View Larger Map')
-    if b_tag:
-        parent_row = b_tag.find_parent('tr')
-        parent_row.decompose()
-    
-    return page
-
-
-def extract(page):
-    """Extract data from the HTML <table> tags inside an input BeautifulSoup4 
-    object, 'page', representing one of the six pages associated with a
-    school_id on CollegeData.com, using pandas.read_html() to convert tables
-    to DataFrames and processing to output the data as a pandas Series object.
-    """
-    page_s = pd.Series()
-    
-    # Convert all HTML tables in page to a list of pandas DataFrames.
-    if page.find('table'):
-        df_list = pd.read_html(page.decode(), na_values = NA_VALS, 
-            index_col = 0)
-    else:
-        df_list = []
-
-    # Split all dfs into separate lists depending on dimension.
-    nocol_df_list = []
-    onecol_df_list = []
-    table_df_list = []
-    for df in df_list:
-        if len(df.columns) == 1 and len(df) > 1:
-            onecol_df_list.append(df)
-        if len(df.columns) > 1:
-            table_df_list.append(df)
-
-    # DATAFRAMES w/ONE COLUMN
-    ##########################################################################
-    # Extract only column from each df in one_col_df_list as a Series.
-    s_list = [df.iloc[:, 0] for df in onecol_df_list]
-    if s_list:
-        s = pd.concat(s_list)
-        page_s = page_s.append(s)
-    
-    # DATAFRAMES w/MULTIPLE COLUMNS
-    ##########################################################################
-    for df in table_df_list:
-        # TABLES WITH UNIQUE VALUES
-        # 'High School Units Required or Recommended' table on 'Admissions' 
-        # page, and 'Examinations' table, also on 'Admissions' page, have the 
-        # same structure. Create Series from cell vals each labeled by 
-        # combining row + column label.
-        if df.index.name in ['Subject', 'Exam']:
-            s = pd.Series()
-            cols_s = [df[col] for col in df.columns]
-            for col_s in cols_s:
-                col_s = col_s[col_s.index.notna()] # drop null index rows
-                if col_s.name.find('Unnamed') == -1:
-                    col_s.index = col_s.index + ', ' + col_s.name
-                s = s.append(col_s)
-            page_s = page_s.append(s)
-            
-        # CATEGORICAL TABLES
-        # 'Selection of Students' tables exist on 'Overview' and 
-        # 'Admissions' page. Each row only contains only up to a single 'X' 
-        # under one of the columns. Create a Series of the marked column label
-        # for each row.
-        if df.index.name == 'Factor':
-            s = pd.Series()
-            for row_name, row in df.iterrows():
-                label = 'Factor, ' + row_name
-                vals = row.dropna().index.tolist()
-                if vals:
-                    val = vals[0]
-                    s[label] = val
-            page_s = page_s.append(s)
-
-        # 'Intercollegiate Sports Offered' exists on the 'Campus Life'
-        # page. It is similar to the previous 'Selection of Students'
-        # table, but we will extract values by column instead of by row.
-        # Also, multiple rows can be marked, so our Series values will be 
-        # a list of marked row labels.
-        col_level_names = ['Sport', 'Offered']
-        if df.columns.names == ['Sport', 'Offered']:
-            # Manually simplify the column names.
-            df.columns = ['Sports, Women, Scholarships Given',
-                          'Sports, Women, Offered',
-                          'Sports, Men, Scholarships Given',
-                          'Sports, Men, Offered']
-            s = pd.Series(index = df.columns)
-            for col_name in df.columns:
-                s[col_name] = df[col_name].dropna().index.tolist()
-            page_s = page_s.append(s)
-            
-    # ADDITIONAL EXTRACTIONS
-    ##########################################################################
-    # Get school Name and Description, which are not in a table.
-    if page.h1 and page.h1.text != EMPTY_H1:
-        page_s['Name'] = page.h1.text
-    if page.p:
-        page_s['Description'] = page.p.text
-
-    # Get the FAFSA code from the strangely formatted 2nd table of section10.
-    div = page.find('div', id='section10')
-    if div:
-        tables = div.find_all('table')
-        if len(tables) == 3:
-            # The FAFSA code is the last six characters of this text.
-            page_s['FAFSA Code'] = tables[2].tbody.th.text[-6:]
-
-    # Get lists of majors / programs of study from strangely formatted tables.
-    caption_strings = ['Undergraduate Majors',
-                       "Master's Programs of Study",
-                       'Doctoral Programs of Study']
-    for caption_string in caption_strings:
-        regex = re.compile(caption_string)
-        caption = page.find('caption', string=regex)
-        if caption:
-            vals = []
-            for tag_name in ['th','td']:
-                tag = caption.find_next(tag_name)
-                if tag:
-                    tag_string = "---".join(tag.stripped_strings)
-                    vals += tag_string.split('---')
-            page_s[caption_string] = vals
-            
-    # Get values from tables with no label (but do have captions!).
-    captions = ['Entrance Difficulty',
-               "Master's Degrees Offered",
-               "Doctoral Degrees Offered"]
-    for caption in captions:
-        regex = re.compile(caption)
-        caption_tag = page.find('caption', string = regex)
-        if caption_tag:
-            val = caption_tag.parent.tbody.th.get_text(' ', strip = True)
-            page_s[caption] = val
-    
-    return page_s
-
-
-def drop_duplicates(s):
-    """For each duplicate index in input pandas Series object 's', this
-    function checks that duplicate index values are equal. If all duplicate
-    index values are equal, duplicate entries in the Series are dropped."""
-    s = s.dropna() # drop labels with null vals
-    
-    first_dups_mask = s.index.duplicated(keep = 'first')
-    last_dups_mask = s.index.duplicated(keep = 'last')
-    
-    first_dups = s[first_dups_mask].sort_index()
-    last_dups = s[last_dups_mask].sort_index()
-    
-    diff_dups = (first_dups != last_dups)
-    
-    msg = f"Some dup labels have diff vals.\n{diff_dups}"
-    assert sum(diff_dups) == 0, msg
-    
-    s = s[~last_dups_mask] # drop last dups, keep first.
-    
+def remove_duplicates(s):
+    # Check for duplicates
+    s = s.dropna()
+    idx_first_dups = s.index.duplicated(keep = 'first')
+    idx_last_dups = s.index.duplicated(keep = 'last')
+    first_dups = s[idx_first_dups].sort_index()
+    last_dups = s[idx_last_dups].sort_index()
+    mask = first_dups != last_dups
+    dups = first_dups[mask].append(last_dups[mask])
+    if not dups.empty:
+        logging.warning('Duplicate values: ' + str(dups))
+        raise Exception
+    school_series = s[~idx_last_dups] # keep only first instances of dups
     return s
 
 
 def main():
-    
-    schools = []
-    exceptions = []
-    
-    for school_id in SCHOOL_IDS:
-
-        # Hold scraped data for all six pages in pandas Series object.
-        school_s = pd.Series()
-
-        try:
-            for page_id in PAGE_IDS:
-
-                # Request and convert page into BeautifulSoup object.
-                page = get_collegedata_page(school_id, page_id)
-
-                # Relabel some HTML tag strings to prevent data loss to dups.
-                page = relabel(page)
-
-                # Extract data from page into a pandas Series object.
-                page_s = extract(page)
-
-                if not page_s.empty:
-                    # Append extracted page Series to total school Series.
-                    school_s = school_s.append(page_s)
-                else:
-                    break
-                    
-        except Exception:
-            exceptions.append([school_id, Exception])
+    # Get start and stop school IDs from user or defaults from config file.
+    try:
+        if len(sys.argv) > 1:
+            start_school_id = int(sys.argv[1])
         else:
-            # Drop duplicate entries if values are also duplicated.
-            school_s = drop_duplicates(school_s)
-            school_s.name = school_id
-            schools.append(school_s)
-        finally:
-            clear_output(wait = True)
+            start_school_id = config['SCHOOL_ID']['START']
+        if len(sys.argv) > 2:
+            end_school_id = int(sys.argv[2])
+        else:
+            end_school_id = config['SCHOOL_ID']['END']
+    except ValueError:
+        print('School IDs must be integers.')
+        raise
 
-    return schools, exceptions
+    # Get data for each school and export to .csv file.
+    try:
+        school_series_list = []
+        for school_id in range(start_school_id, end_school_id + 1):
+            try:
+                school_series = scrape_school(school_id)
+                if school_series is not None:
+                    school_series.name = school_id
+                    school_series_list.append(school_series)
+            except IOError:
+                pass
+    except KeyboardInterrupt:
+        print('Stopping...')
+    except Exception as e:
+        msg = f'ERROR: Exception encountered.'
+        logging.critical(msg, exc_info = True)
+    else:
+        print('Successfully finished.')
+    finally:
+        df = pd.DataFrame(school_series_list)
+        path = config['PATHS']['CSV']
+        df.to_csv(path)
+        print(f'Scraped {len(df)} schools to {path}')
+
+
+if __name__ == '__main__':
+    main()
